5b66191ade8ca8d389b27c535fd751b4a66be31e
Erlisja Kore <erlisja.kore@temple.edu>
2025-01-02 19:05:58 -0500

    added a paragraph component

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Paragraph() {
+  return (
+    <>
+    <div>Paragraph</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi et cum vitae? 
+        Illum, cum accusantium. Aliquid quas, 
+        hic rerum praesentium architecto unde ullam, fuga exercitationem 
+        laboriosam facilis magni vero voluptatum.
+        </p>
+    </>
+  )
+}
+
+export default Paragraph
